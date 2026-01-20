21683d0dbcd24641f1d7ec68a9c579f44f750405
amangupta <amangupta954055@gmail.com>
2026-01-21 00:46:32 +0530

    feat: implemented the CORS and setup the config enviroment

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,8 @@ dotenv.config();
 
 const config = {
   PORT: process.env.PORT || 8080,
+  NODE_ENV: process.env.NODE_ENV,
+  WHITELIST_ORIGINS: ['https://docs.blog-api.codewithsadee.com'],
 };
 
 export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,53 @@
 import express from 'express';
+import cors from 'cors';
+
+/**
+ * Custom modules
+ */
+
+import config from '@/config';
+
+/**
+ * Types
+ */
+
+import type { CorsOptions } from 'cors';
+
+/**
+ * Express app initial
+ */
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('server is running on port: 3000 ');
+// Configure CORS options
+
+const corsOptions: CorsOptions = {
+  origin(requestOrigin, callback) {
+    if (
+      config.NODE_ENV === 'development' ||
+      !requestOrigin ||
+      config.WHITELIST_ORIGINS.includes(requestOrigin)
+    ) {
+      callback(null, true);
+    } else {
+      // Rejects requests from non-whitelisted origins
+      callback(
+        new Error(`CORS error: ${requestOrigin} is not allowed by CORS`),
+        false
+      );
+    }
+    console.log(`CORS error: ${requestOrigin} is not allowed by CORS`);
+  },
+};
+
+// Apply CORS Middleware
+app.use(cors({ origin: false }));
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello from backend!',
+  });
+});
+app.listen(config.PORT, () => {
+  console.log('server is running on port: ', config.PORT);
 });
